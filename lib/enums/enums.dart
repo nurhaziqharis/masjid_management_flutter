a532bd8372ac9237d7ed862c525305f01f29518b
Haziq Haris <muhammadnurhaziqharis@yahoo.com>
2025-09-05 22:14:50 +0800

    Add switch case to change enum value before send API

--- lib/enums/enums.dart
+++ lib/enums/enums.dart
@@ -5,3 +5,20 @@ enum Category {
   Routine,
   Seminar,
 }
+
+extension CategoryExtension on Category {
+  String toApiValue() {
+    switch (this) {
+      case Category.EMPTY:
+        return "Empty";
+      case Category.Festival:
+        return "Festival";
+      case Category.KnowledgeSharing:
+        return "Knowledge Sharing";
+      case Category.Routine:
+        return "Routine";
+      case Category.Seminar:
+        return "Seminar";
+    }
+  }
+}
